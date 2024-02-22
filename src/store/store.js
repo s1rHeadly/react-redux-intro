f5e08f1a8bcc70bdd044a80a858bdb701b3a65c2
davidmchale <dmchale@squiz.net>
2024-02-22 14:25:00 +1100

    action creators for the reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,4 @@
+import { createStore } from "redux"
 
 
 // set initial state
@@ -31,7 +32,9 @@ const reducer = (state = initialState, action) => { // we need to set the state 
            // later
            return{
             ...state,
-            loan: action.payload
+            loan: action.payload.amount,
+            loanPurpose: action.payload.purpose,
+            balance: state.balance + action.payload.amount
            }
 
            //loan payback
@@ -50,3 +53,73 @@ const reducer = (state = initialState, action) => { // we need to set the state 
 }
 
 
+const store = createStore(reducer);
+
+
+// use our dispatch our actions here
+// store.dispatch({
+//   type: 'account/deposit',
+//   payload: 500,
+// })
+
+
+// store.dispatch({
+//   type: 'withdraw',
+//   payload: 300,
+// })
+
+function deposit(amount){
+  return{
+      type: 'account/deposit',
+      payload: amount,
+  }
+}
+
+
+
+function withdraw(amount){
+  return{
+    type: 'withdraw',
+    payload: amount,
+  }
+}
+
+
+function requestLoan(amount, reason){
+  return{
+    type: 'account/requestLoan',
+    payload: {
+      amount: amount,
+      purpose: reason
+      }
+    }
+}
+
+
+function payLoan(){
+  return{
+    type: 'account/payLoan'
+  }
+}
+
+
+
+//test the file renders when we have it imported in the index.js file
+// 1. console.log('store rendered')
+
+
+// 2. log the store to get all state values
+console.log(store.getState())
+
+
+// 3. add action creators to the store
+store.dispatch(deposit(500)) // dispatch the deposit function to the store like this...
+store.dispatch(withdraw(200)) // same for withdraw function etc ....
+store.dispatch(requestLoan(3000, 'To buy a car'))
+
+console.log(store.getState()) // then test it works again
+
+store.dispatch(payLoan());
+
+
+console.log(store.getState()) // then test it works again
